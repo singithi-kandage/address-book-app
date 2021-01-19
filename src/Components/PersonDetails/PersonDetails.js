31b13bf0b53e535d811b1c0dbdf3256440162a65
Singithi Kandage <singithi.kandage@gmail.com>
2021-01-19 16:28:21 -0500

    Initial implementation of details page

--- src/Components/PersonDetails/PersonDetails.js
+++ src/Components/PersonDetails/PersonDetails.js
@@ -1,5 +1,43 @@
+import { Fragment } from "react";
+import { useSelector } from "react-redux";
+import FormField from "../../Shared/FormField/FormField";
+
 const PersonDetails = () => {
-  return <p>person details</p>;
+  const person = useSelector(state => state.person);
+  console.log(person);
+  return (
+    <Fragment>
+      {person.firstName !== "" &&
+        <Fragment>
+          <h2>{`${person.firstName} ${person.lastName}`}</h2>
+
+          <img
+            src={person.imageUrl}
+            alt={`${person.firstName} ${person.lastName}`}
+          />
+
+          <form className="form">
+            <FormField
+              labelText="First Name"
+              inputType="text"
+              inputValue={person.firstName}
+            />
+
+            <FormField
+              labelText="Last Name"
+              inputType="text"
+              inputValue={person.lastName}
+            />
+
+            <FormField
+              labelText="Phone Number"
+              inputType="text"
+              inputValue={person.phoneNumber}
+            />
+          </form>
+        </Fragment>}
+    </Fragment>
+  );
 };
 
 export default PersonDetails;
