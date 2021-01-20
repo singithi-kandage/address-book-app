fe45a5c382de9fbbec85cd59a973f451864f1595
Singithi Kandage <singithi.kandage@gmail.com>
2021-01-19 19:09:26 -0500

    Completed page structure

--- src/Components/PersonDetails/PersonDetails.js
+++ src/Components/PersonDetails/PersonDetails.js
@@ -1,40 +1,56 @@
 import { Fragment } from "react";
 import { useSelector } from "react-redux";
-import FormField from "../../Shared/FormField/FormField";
+
+import { history } from "../../index";
+import { Form, FormGroup, FormSection } from "../../Shared/Form/Form";
+import { ButtonContainer, Button } from "../../Shared/Button/Button";
 
 const PersonDetails = () => {
   const person = useSelector(state => state.person);
-  console.log(person);
+
   return (
     <Fragment>
       {person.firstName !== "" &&
         <Fragment>
           <h2>{`${person.firstName} ${person.lastName}`}</h2>
 
-          <img
-            src={person.imageUrl}
-            alt={`${person.firstName} ${person.lastName}`}
-          />
+          <Form>
+            <FormSection customWidth={"30%"}>
+              <img
+                className="form_formImage"
+                src={person.imageUrl}
+                alt={`${person.firstName} ${person.lastName}`}
+              />
+            </FormSection>
 
-          <form className="form">
-            <FormField
-              labelText="First Name"
-              inputType="text"
-              inputValue={person.firstName}
-            />
+            <FormSection customWidth={"70%"}>
+              <FormGroup
+                labelText="First Name"
+                inputType="text"
+                inputValue={person.firstName}
+              />
 
-            <FormField
-              labelText="Last Name"
-              inputType="text"
-              inputValue={person.lastName}
-            />
+              <FormGroup
+                labelText="Last Name"
+                inputType="text"
+                inputValue={person.lastName}
+              />
 
-            <FormField
-              labelText="Phone Number"
-              inputType="text"
-              inputValue={person.phoneNumber}
+              <FormGroup
+                labelText="Phone Number"
+                inputType="text"
+                inputValue={person.phoneNumber}
+              />
+            </FormSection>
+          </Form>
+
+          <ButtonContainer position="flex-end">
+            <Button
+              buttonText={"Back to Results"}
+              buttonClass={"primary"}
+              onClick={() => history.push("/", { from: "PersonDetails" })}
             />
-          </form>
+          </ButtonContainer>
         </Fragment>}
     </Fragment>
   );
