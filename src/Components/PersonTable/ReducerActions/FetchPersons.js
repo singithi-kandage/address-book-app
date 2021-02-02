045df6dfa0e82086f73fdcc777ecf86b9154e231
Singithi Kandage <kandasi@americas.manulife.net>
2021-02-01 20:15:35 -0500

    Switched to axios

--- src/Components/PersonTable/ReducerActions/FetchPersons.js
+++ src/Components/PersonTable/ReducerActions/FetchPersons.js
@@ -1,42 +1,46 @@
+import axios from "axios";
 import produce from "immer";
 import { registerReducer } from "../../../StateSetup/RootReducer";
 
-const BASE_RANDOM_USERS_URL = process.env.REACT_APP_BASE_RANDOM_USERS_URL;
 const OK = 200;
 
 export const RESPONSE_ACTION_NAME = "ACTION_FETCH_PEOPLE_RESPONSE";
 export const ERROR_ACTION_NAME = "ACTION_FETCH_PEOPLE_ERROR";
 
-export const FetchPersons = page => {
-  return async dispatch => {
-    const response = await fetch(
-      `${BASE_RANDOM_USERS_URL}/?page=${page}&results=10&seed=abc&?exc=login`,
-      {
-        method: "GET",
-        headers: {
-          "Content-Type": "text/plain",
-          "Access-Control-Allow-Origin": "https://randomuser.me",
-        },
-      }
-    );
+export const FetchPersons = (page) => {
+  return async (dispatch) => {
+    try {
+      axios
+        .get(`/api/?page=${page}&results=10&seed=abc&?exc=login`, {
+          headers: {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "http://localhost:3000",
+          },
+        })
+        .then((response) => {
+          if (response.status === OK) {
+            const data = response.data.results;
 
-    if (response.status === OK) {
-      const responseBody = await response.json();
-
-      const personList = responseBody.results.map(result => {
-        return {
-          firstName: result.name.first,
-          lastName: result.name.last,
-          phoneNumber: result.phone,
-          imageUrl: result.picture.large,
-        };
-      });
-
-      dispatch({
-        type: RESPONSE_ACTION_NAME,
-        personList,
-      });
-    } else {
+            const personList = data.map((result) => {
+              return {
+                firstName: result.name.first,
+                lastName: result.name.last,
+                phoneNumber: result.phone,
+                imageUrl: result.picture.large,
+              };
+            });
+            dispatch({
+              type: RESPONSE_ACTION_NAME,
+              personList,
+            });
+          } else {
+            dispatch({
+              type: ERROR_ACTION_NAME,
+            });
+          }
+        });
+    } catch (error) {
+      console.error(error);
       dispatch({
         type: ERROR_ACTION_NAME,
       });
